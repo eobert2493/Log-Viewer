d0dc71eeaa6f71f629b5685d66ea8c582f01a2be
Eric-MBP <eobert2493@gmail.com>
2023-12-29 23:14:33 -0600

    added line number in left margins

--- LogViewer.py
+++ LogViewer.py
@@ -3,10 +3,68 @@ import xml.dom.minidom
 from xml.parsers.expat import ExpatError
 import re
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTextEdit, QPushButton, QFileDialog, QSplitter
-from PyQt5.QtGui import QTextOption, QDragEnterEvent, QDropEvent, QFont, QTextCursor
+from PyQt5.QtGui import QTextOption, QDragEnterEvent, QDropEvent, QFont, QTextCursor, QColor, QDragMoveEvent
 from PyQt5.QtCore import Qt
 from PyQt5.Qsci import QsciScintilla, QsciLexerPython, QsciLexerJSON, QsciLexerXML
 
+class InputScintilla(QsciScintilla):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # Enable line highlighting
+        self.setCaretLineVisible(True)
+        self.setCaretLineBackgroundColor(QColor("#E8E8E8"))
+        
+        # Enable line numbers
+        self.setMarginType(0, QsciScintilla.NumberMargin)
+        self.setMarginWidth(0, "000")
+        self.setMarginLineNumbers(0, True)
+        
+        # Set margins font
+        font = QFont('Courier New')
+        self.setMarginsFont(font)
+
+        # Enable drag and drop
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, event: QDragEnterEvent):
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+
+    def dragMoveEvent(self, event: QDragMoveEvent):
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+
+    def dropEvent(self, event: QDropEvent):
+        if event.mimeData().hasUrls():
+            # Handle file drop
+            url = event.mimeData().urls()[0]
+            file_path = url.toLocalFile()
+            self.import_text(file_path)
+
+    def import_text(self, file_path):
+        with open(file_path, 'r') as file:
+            self.setText(file.read())
+
+class OutputScintilla(QsciScintilla):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # Enable line highlighting
+        self.setCaretLineVisible(True)
+        self.setCaretLineBackgroundColor(QColor("#E8E8E8"))
+        
+        # Enable line numbers
+        self.setMarginType(0, QsciScintilla.NumberMargin)
+        self.setMarginWidth(0, "000")
+        self.setMarginLineNumbers(0, True)
+        
+        # Enable code folding
+        self.setFolding(QsciScintilla.BoxedTreeFoldStyle)
+        
+        # Set margins font
+        font = QFont('Courier New')
+        self.setMarginsFont(font)
 
 class DragDropTextEdit(QTextEdit):
     def __init__(self, parent=None):
@@ -23,29 +81,27 @@ class DragDropTextEdit(QTextEdit):
             self.setText(file.read())
 
 def shorten_and_sort_logs():
-    log_lines = input.toPlainText().split('\n')
+    log_lines = input.text().split('\n')
     for i in range(len(log_lines)):
         match = re.search(r'.*\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3})\].*?\[(INFO|ERROR)\](.*)', log_lines[i])
         if match:
             log_lines[i] = f'[{match.group(1)}] [{match.group(2)}]{match.group(3)}'
     log_lines.sort()
-    input.setPlainText('\n'.join(log_lines))
-
-json_lexer = QsciLexerJSON()
-xml_lexer = QsciLexerXML()
-font = QFont()
-font.setFamily('Courier New')
-font.setFixedPitch(True)
-font.setPointSize(12)
-xml_lexer.setFont(font)
-json_lexer.setFont(font)
+    input.setText('\n'.join(log_lines))
 
 def pretty_print_line():
-    cursor = input.textCursor()
-    cursor.movePosition(QTextCursor.StartOfLine)
-    cursor.movePosition(QTextCursor.EndOfLine, QTextCursor.KeepAnchor)
-    log_line = cursor.selectedText()
+    json_lexer = QsciLexerJSON()
+    xml_lexer = QsciLexerXML()
+    font = QFont()
+    font.setFamily('Courier New')
+    font.setFixedPitch(True)
+    font.setPointSize(12)
+    json_lexer.setFont(font)
+    xml_lexer.setFont(font)
     
+    line, _ = input.getCursorPosition()
+    log_line = input.text(line)
+
     # Try to find JSON in the log line
     json_match = re.search(r'\{.*\}', log_line)
     if json_match:
@@ -65,7 +121,7 @@ def pretty_print_line():
         xml_str = xml_match.group()
         try:
             xml_obj = xml.dom.minidom.parseString(xml_str)
-            pretty_xml = xml_obj.toprettyxml(indent='       ')
+            pretty_xml = xml_obj.toprettyxml(indent='     ')
             output.setText(pretty_xml)
             output.setLexer(xml_lexer)
             return
@@ -79,7 +135,7 @@ def import_text():
     file_name, _ = QFileDialog.getOpenFileName(main_window, "Open Text File", "", "Text Files (*.txt)")
     if file_name:
         with open(file_name, 'r') as file:
-            input.setPlainText(file.read())
+            input.setText(file.read())
 
 app = QApplication([])
 
@@ -108,13 +164,13 @@ splitter.setStyleSheet("""
     }
 """)
 
-input = DragDropTextEdit(main_window)
-input.setWordWrapMode(QTextOption.NoWrap)
+input = InputScintilla(main_window)
+input.setWrapMode(QsciScintilla.WrapNone)
 input.cursorPositionChanged.connect(pretty_print_line)
 input.setFont(QFont('Courier New'))
 splitter.addWidget(input)
 
-output = QsciScintilla(main_window)
+output = OutputScintilla(main_window)
 output.setWrapMode(QsciScintilla.WrapNone)
 output.setFont(QFont('Courier New'))
 splitter.addWidget(output)
